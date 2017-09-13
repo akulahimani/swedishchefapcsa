fbea559c9921b6e0f8d99689fc44e49c411652ba
SeanMccor <31771080+SeanMccor@users.noreply.github.com>
2017-09-13 10:25:56 -0400

    Update Swedish_Chef.java

--- src/Swedish_Chef.java
+++ src/Swedish_Chef.java
@@ -29,72 +29,51 @@ public class Swedish_Chef {
 							endString += "ZEE";
 							x++;
 							x++;
-							y = '*';
 						}
 					}
 				}
-			}
-			
-			if(y== 'A') {//AN to UN #3
+			}else if(y== 'A') {//AN to UN #3
 				if(x + 1 < first.length()) {
 					if(first.charAt(x+1) == 'N') {
 						endString += "UN";
 						x++;
-						y = '*';
 					}
 				}
-			}
-			
-			if(y== 'A') {//AU to OO #4
+			}else if(y== 'A') {//AU to OO #4
 				if(x + 1 < first.length()) {
 					if(first.charAt(x+1) == 'U') {
 						endString += "OO";
 						x++;
-						y = '*';
 					}
 				}
-			}
-			if(y == 'A') {//A to E #5
+			}else if(y == 'A') {//A to E #5
 				if(x + 1 < first.length()) {
 					if(first.charAt(x+1) != ' ') {
 						endString += "E";
-						y = '*';
 					}
 				}
-			}
-			
-			if(y == 'O') {//OW to OO #6
+			}else if(y == 'O') {//OW to OO #6
 				if(x + 1 < first.length()) {
 					if(first.charAt(x+1) == 'W') {
 						endString += "OO";
 						x++;
-						y = '*';
 					}
 				}
-			}
-			
-			if(y == 'O') {//O to U #7
+			}else if(y == 'O') {//O to U #7
 				endString += "U";
-				y = '*';
-			}
-			
-			if(y == 'I') {//IR to UR #8
+			}else if(y == 'I') {//IR to UR #8
 				if(x + 1 < first.length()) {
 					if(first.charAt(x+1) == 'R') {
 						endString += "UR";
 						x++;
-						y = '*';
 					}
 				}
-			}
-			
-			if(y == 'T') {//TION to SHUN #9
+			}else if(y == 'T') {//TION to SHUN #9
 				if(x + 3 < first.length()) {
 					if(first.charAt(x+1) == 'I') {
 						if(first.charAt(x+2) == 'O') {
 							if(first.charAt(x+3) == 'N') {
 								endString += "SHUN";
-								y = '*';
 								x++;
 								x++;
 								x++;
@@ -102,9 +81,7 @@ public class Swedish_Chef {
 						}
 					}
 				}
-			}
-			
-			if(y == 'I') {//I to EE if first I in word #10 but not at Start of word
+			}else if(y == 'I') {//I to EE if first I in word #10 but not at Start of word
 				if(x-1 == -1) {
 				}else {
 					if(first.charAt(x-1) != ' ') {
@@ -121,57 +98,39 @@ public class Swedish_Chef {
 						}
 						if(o == 1) {
 							endString += "EE";
-							y = '*';
 						}
 					}	
 				}
-			}
-			
-			if(y == 'E') {//EN to EE at end of word #11
+			}else if(y == 'E') {//EN to EE at end of word #11
 				if(x + 2 < first.length()) {
 					if(first.charAt(x+1) == 'N') {
 						if(first.charAt(x+2) == ' ') {
 							x++;
 							x++;
-							y = '*';
 							endString += "EE ";
 						}
 					}
 				}
-			}
-			
-			if(y == 'F') {//F to FF #12
-				y = '*';
+			}else if(y == 'F') {//F to FF #12
 				endString += "FF";	
-			}
-			
-			if(y == 'E') {//E with E-A and end of word #13
+			}else if(y == 'E') {//E with E-A and end of word #13
 				if(x + 1 < first.length()) {
 					if(first.charAt(x+1) == ' ') {
 						endString += "E-A ";
 						x++;
-						y = '*';
 					}
 				}
-			}
-			
-			if(y == 'U') {//U to OO except at start of word  #14
+			}else if(y == 'U') {//U to OO except at start of word  #14
 				if(x-1 != -1) {
 					if(first.charAt(x-1) != ' ') {
 						endString += "OO";
-						y = '*';
 					}
 				}
-			}
-			if(y == 'V') {//V with F #15
+			}else if(y == 'V') {//V with F #15
 				endString += "F";
-				y = '*';
-			}
-			if(y == 'W') {//W with V #16
+			}else if(y == 'W') {//W with V #16
 				endString += "V";
-				y = '*';
-			}
-			if(y != '*') {
+			}else{
 				endString += y;
 			}
 		}
